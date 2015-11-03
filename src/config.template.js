8028f1e282e8faec11cea27fae9a3c5a8064b5fd
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-11-03 14:45:17 -0600

    Fixed config missing form paths

--- src/config.template.js
+++ src/config.template.js
@@ -1,9 +1,11 @@
 angular.module('formioPrizeDrawing').constant('AppConfig', {
 {% if domain %}
   appUrl: '{{ domain }}',
-  apiUrl: '{{ domain }}'
+  apiUrl: '{{ domain }}',
 {% else %}
   appUrl: '{{ protocol }}://{{ path }}.{{ host }}',
-  apiUrl: '{{ protocol }}://api.{{ host }}'
+  apiUrl: '{{ protocol }}://api.{{ host }}',
 {% endif %}
+  adminLoginPath: 'admin/login',
+  drawingFormPath: 'prize'
 });
