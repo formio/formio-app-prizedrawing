6ec99993e7f450c60487048c5c1da11706b8540d
Travis Tidwell <travis@form.io>
2016-01-03 20:50:29 -0600

    Updating gh-pages.

--- config.js
+++ config.js
@@ -1,6 +1,15 @@
+var APP_URL = 'http://oentoewywpwrihd.localhost:3000';
+var API_URL = 'http://api.localhost:3000';
+
+// Parse query string
+var query = {};
+location.search.substr(1).split("&").forEach(function(item) {
+    query[item.split("=")[0]] = item.split("=")[1] && decodeURIComponent(item.split("=")[1]);
+});
+
 angular.module('formioPrizeDrawing').constant('AppConfig', {
-  appUrl: 'https://formio.form.io',
-  apiUrl: 'https://api.form.io',
+  appUrl: query.appUrl || APP_URL,
+  apiUrl: query.apiUrl || API_URL,
   adminLoginPath: 'admin/login',
-  drawingFormPath: 'nodeinteractive'
+  drawingFormPath: 'prize'
 });
